a64ee601a346285ee723a59effa0665a8c5b382d
Spyros Gourgaris <77027954+Spygour@users.noreply.github.com>
2025-05-15 10:10:09 +0300

    Update SpiSlaveTypes.vhd

--- FpgaGraphicsDriver/DataHandler/Spi/SpiSlaveTypes.vhd
+++ FpgaGraphicsDriver/DataHandler/Spi/SpiSlaveTypes.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_signed.all;
 
 package SpiSlaveTypes is
-    subtype Spi_SpiWord is std_logic_vector(0 to 31);
-    subtype Spi_SpiCorrected is std_logic_vector(31 downto 0);
+    subtype Spi_SpiWord is std_logic_vector(0 to 15);
+    subtype Spi_SpiCorrected is std_logic_vector(15 downto 0);
     subtype Spi_QSpiWord is array(0 to 2) of Spi_SpiWord;
     subtype Spi_QSpiCorrected is array(0 to 2) of Spi_SpiCorrected;
 	 
